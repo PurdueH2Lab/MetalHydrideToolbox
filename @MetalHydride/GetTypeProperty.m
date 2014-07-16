4e49dab4ab52c61a9ab7149a3c590431354eb5f7
Essene <waters5@purdue.edu>
2014-07-16 10:56:27 -0400

    stuff

--- @MetalHydride/GetTypeProperty.m
+++ @MetalHydride/GetTypeProperty.m
@@ -36,54 +36,132 @@ function value = GetTypeProperty(propName,type)
     switch propName
         
         %-----------------------------------------------------------------
-        case 'Tc'
-            switch type
-                case 'AB5'
-                    value = 500; %K
-                case 'AB2'
-                    value = 370; %K
-                otherwise
-                    value = 600; %K
-            end
-            
+%         case 'Tc'
+%             switch type
+%                 case 'AB5'
+%                     value = 500; %K
+%                 case 'AB2'
+%                     value = 370; %K
+%                 otherwise
+%                     value = 600; %K
+%             end
+            case 'Tc'
+                switch type
+                    case 'AB5'
+                        value = 586; %K
+                    case 'AB2'
+                        value = 451; %K
+                    case 'AB'
+                        value = 502; %K
+                    case 'AB3'
+                        value = 343; %K
+                    case 'Complex'
+                        value = 2330; %K
+                    case 'MG'
+                        value = 663; %K
+                    case 'SS'
+                        value = 473; %K
+                    otherwise
+                        value = 600; %K
+              end
+
         %-----------------------------------------------------------------
+%         case 'Slope' %assumed for desorption
+%             switch type
+%                 case 'AB5'
+%                     value = 0.5;
+%                 case 'AB2'
+%                     value = 0.64;
+%                 case 'AB'
+%                     value = 0.5;
+%                 otherwise
+%                     value = 0;
+%             end
         case 'Slope' %assumed for desorption
-            switch type
-                case 'AB5'
-                    value = 0.5;
-                case 'AB2'
-                    value = 0.64;
-                case 'AB'
-                    value = 0.5;
-                otherwise
-                    value = 0;
-            end
+                    switch type
+                        case 'AB'
+                            value = 0.92;
+                        case 'AB2'
+                            value = 1.25;
+                        case 'AB3'
+                            value = 1.2;  
+                        case 'AB5'
+                            value = 0.43;
+                        case 'MG'
+                            value = 0.02;
+                        case 'SS'
+                            value = 0.33;                      
+                        otherwise
+                            value = 0;
+                end
             
         %-----------------------------------------------------------------
-        case 'Hysteresis'
-            switch type
-                case 'AB5'
-                    value = 0.2;
-                case 'AB2'
-                    value = 0.4;
-                case 'AB'
-                    value = 0.4;
-                otherwise
-                    value = 0;
-            end
+%         case 'Hysteresis'
+%             switch type
+%                 case 'AB5'
+%                     value = 0.2;
+%                 case 'AB2'
+%                     value = 0.4;
+%                 case 'AB'
+%                     value = 0.4;
+%                 otherwise
+%                     value = 0;
+%             end
+            case 'Hysteresis' %assumed for desorption
+                    switch type
+                        case 'AB'
+                            value = 0.51;
+                        case 'AB2'
+                            value = 0.54;
+                        case 'AB3'
+                            value = 0.29;  
+                        case 'AB5'
+                            value = 0.39;
+%                         case 'Mg'
+%                             value = 3.71;
+                        case 'SS'
+                            value = 0.80;                      
+                        otherwise
+                            value = 0;
+                end
             
         %-----------------------------------------------------------------
-        case 'Ca Hydriding'
-            switch type
-                case 'MG'
-                    value = 9.8e9; % DOI 10.1016/j.ijhydene.2010.03.057
-                otherwise
-                    value = 57;
-            end
+%         case 'Ca Hydriding'
+%             switch type
+%                 case 'MG'
+%                     value = 9.8e9; % DOI 10.1016/j.ijhydene.2010.03.057
+%                 otherwise
+%                     value = 57;
+%             end
+            case 'Ca Hydriding' %assumed for desorption
+                    switch type
+                        case 'AB'
+                            value = 7523;
+                        case 'AB2'
+                            value = 473;
+                        case 'AB3'
+                            value = 24.4;  
+                        case 'AB5'
+                            value = 620;
+                        case 'Complex'
+                            value = 2e9;
+                        case 'MG'
+                            value = 9.8e9;                      
+                        otherwise
+                            value = 57;
+                end
             
         %-----------------------------------------------------------------
         case 'Ca Dehydriding'
             switch type
+                case 'AB'
+                    value = 99;
+                case 'AB2'
+                    value = 58;
+                case 'AB5'
+                    value = 90;
+                case 'Complex'
+                    value = 1.8e9;
                 case 'MG'
                     value = 10; % DOI 10.1016/j.ijhydene.2010.03.057
                 otherwise
@@ -93,8 +171,18 @@ function value = GetTypeProperty(propName,type)
         %-----------------------------------------------------------------
         case 'Ea Hydriding'
             switch type
+                case 'AB'
+                    value = 40038;
+                case 'AB2'
+                    value = 24900;
+                case 'AB3'
+                    value = 25730;
+                case 'AB5'
+                    value = 30867;
                 case 'MG'
                     value = 132000; % DOI 10.1016/j.ijhydene.2010.03.057
+                case 'Complex'
+                    value = 91520;
                 otherwise
                     value = 20000;
             end
@@ -102,6 +190,14 @@ function value = GetTypeProperty(propName,type)
         %-----------------------------------------------------------------
         case 'Ea Dehydriding'
             switch type
+                case 'AB'
+                    value = 30161;
+                case 'AB2'
+                    value = 22100;
+                case 'AB5'
+                    value = 28250;
+                case 'Complex'
+                    value = 93070;
                 case 'MG'
                     value = 41000; % DOI 10.1016/j.ijhydene.2010.03.057
                 otherwise
@@ -110,13 +206,33 @@ function value = GetTypeProperty(propName,type)
             
         %-----------------------------------------------------------------
         case 'kEff'
-            value = 1;
+            switch type
+                case 'AB2'
+                    value = 1.6;
+                case 'AB5'
+                    value = 1.6;
+                case 'Complex'
+                    value = 0.42;
+                case 'MG'
+                    value = 0.48;
+                otherwise
+                    value = 1;
+            end
+            
             
         %-----------------------------------------------------------------
         case 'Cp'
             switch type
+                case 'AB'
+                    value = 502;
+                case 'AB2'
+                    value = 419;
+                case 'AB5'
+                    value = 419;
+                case 'Complex'
+                    value = 1075;
                 case 'MG'
-                    value = 1000;
+                    value = 889;
                 otherwise
                     value = 500;
             end
@@ -124,10 +240,16 @@ function value = GetTypeProperty(propName,type)
         %-----------------------------------------------------------------
         case 'rho'
             switch type
+                case 'AB'
+                    value = 6500;
+                case 'AB2'
+                    value = 6700;
                 case 'AB5'
-                    value = 7200;
-                case 'MG'
+                    value = 8200;
+                case 'Complex'
                     value = 1800;
+                case 'MG'
+                    value = 1750;
                 otherwise
                     value = 6600;
             end
